5e2e4a2870977e713b71b3bffbd6b28c23845f4a
reneguerra <rene.guerra@globalr.net>
2012-08-16 17:34:46 -0430

    cambio de rene

--- HolaMundo2/src/Main.java
+++ HolaMundo2/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		System.out.println("hola mundo2");
+		System.out.println("hola mundo23");
 
 	}
 
